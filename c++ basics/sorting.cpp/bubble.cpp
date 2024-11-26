d750364841c360cb93f9f90eec8d56b2a394a6a4
chetan <vaishnavchetan701@gmail.com>
2024-11-26 22:11:57 +0530

    files are updated

--- c++ basics/sorting.cpp/bubble.cpp
+++ c++ basics/sorting.cpp/bubble.cpp
@@ -2,17 +2,23 @@
 using namespace std;
 
 void bubble_sort(int n, int arr[]){
+    int didSwap=0;
     for (int i = n - 1; i > 0; i--){
-        int didSwap=0;
         for (int j = 0; j < i; j++){
             if (arr[j] > arr[j+1]){
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                didSwap++;
             }    
+        }
+        if(didSwap==0){
+            cout<<"\nalready in sorted order no need to sort...";
+            return ;
         }
     }
     for(int i=0;i<n;i++)cout<<arr[i]<<endl;
+    cout<<"Times swaped: "<<didSwap;
 }
 int main()
 {
