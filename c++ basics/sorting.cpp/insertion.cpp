425f62839facb3111adb9763632fcc88481fc1fc
chetan <vaishnavchetan701@gmail.com>
2024-11-27 23:17:47 +0530

    new files are added

--- c++ basics/sorting.cpp/insertion.cpp
+++ c++ basics/sorting.cpp/insertion.cpp
@@ -1,16 +1,25 @@
+//Explaination:- basically we are taking element and putting them to their original positon
+//ex:- 4,5,3,2,6
+//in first loop we take 0th element
+//2 -- we take 0th and 1st element of array and place 1st element at his right place
+//3 -- we take 0,1,2 index and place 2nd element at his right place
+//outer loop will run for i=0 to i<sizeofarray, inner loop will run for j=i to j=0
+
+
 #include<bits/stdc++.h>
 using namespace std;
 void insertion(int x, int arr[]){
-    for(int i=0;i<x;i++){
-        int max=i;
-        for(int j=i;j<x;j++){
-            if(arr[max]>arr[j])max=j;
+    for(int i=0;i<=x-1;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j-1]>arr[j]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                cout<<"runs\n";
+            }
         }
-        int temp = arr[i];
-        arr[i] = arr[max];
-        arr[max] = temp;
     }
-       
+    for(int i= 0;i<x;i++){cout<<arr[i]<<endl;}
 }
 
 int main(){
@@ -19,5 +28,5 @@ int main(){
     int arr[x];
     for(int i=0;i<x;i++)cin>>arr[i];
     insertion(x,arr);
-    for(int i=0;i<x;i++)cout<<arr[i]<<endl; 
+    
 }
