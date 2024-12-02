d1de1848159af33647618143b844f2350861f4fa
chetan <vaishnavchetan701@gmail.com>
2024-12-02 23:20:32 +0530

    new solution are added

---
+++ leetcode/1455.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int isPrefixOfWord(string sentence, string searchWord) {
+        int wordIndex = 1;  // Words are 1-indexed
+        int n = sentence.size();
+        int searchLen = searchWord.size();
+
+        for (int i = 0; i < n; i++) {
+            // Find the start of a word
+            if (i == 0 || sentence[i - 1] == ' ') {
+                int j = 0;  // Pointer to compare searchWord
+                int k = i;  // Pointer to compare sentence's current word
+                
+                // Check if searchWord matches the prefix
+                while (j < searchLen && k < n && sentence[k] == searchWord[j]) {
+                    j++;
+                    k++;
+                }
+                
+                // If the whole searchWord matched as prefix
+                if (j == searchLen) {
+                    return wordIndex;
+                }
+
+                wordIndex++;  // Move to the next word
+            }
+        }
+
+        return -1;  // No prefix match found
+    }
+};
---
+++ leetcode/tmep.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        // Mapping of Roman numerals to their values
+        unordered_map<char, int> romanMap = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        
+        int result = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            // If the current numeral is less than the next one, subtract it
+            if (i < n - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+                result -= romanMap[s[i]];
+            } else {
+                // Otherwise, add it
+                result += romanMap[s[i]];
+            }
+        }
+        
+        return result;
+    }
+};
