1955d472cc39a6fc0dd1c994b8f565a451ca87ab
chetan <vaishnavchetan701@gmail.com>
2024-12-03 23:55:37 +0530

    files are updated

---
+++ c++ basics/arrays/removeDup.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[]= {1,1,1,3,3,3,5,5,5,7,7,7,7,7,7,8};
+    int i =0;
+    for(int j=1;j<16;j++){
+        if(arr[i]!=arr[j]){
+            arr[i+1]=arr[j];
+            i++;
+        }
+    }
+    for(int j=0;j<=i;j++){
+        cout<<arr[j]<<" ";
+    }
+}
