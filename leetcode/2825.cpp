c80dfddea79acf3d4ffb38dcfd5a087de481c21d
chetan <vaishnavchetan701@gmail.com>
2024-12-04 19:42:16 +0530

    files are updated with better solution

--- leetcode/2825.cpp
+++ leetcode/2825.cpp
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool checkSubSeq(string str1, string str2){
-    int left=0;
-    while(left<str1.size()){
-        int right=0;
-        while(right<str2.size()){
-            if(++str1[left]==str2[right]){
-                return true;
-            }
-            else{
-                right++;
+bool checkSubSeq(string str1, string str2) {
+   int n = str1.size(), m = str2.size();
+        int j = 0;
+        
+        for (int i = 0; i < n && j < m; ++i) {
+            if (str1[i] == str2[j]) {
+                ++j;
+            } 
+            else if ((str1[i] == 'z' && 'a' == str2[j]) || (str1[i] + 1 == str2[j])) {
+                ++j;
             }
         }
-        left++;
-    }
-    return false;
+        
+        return j == m;
 }
 
-int main(){
-    string str1="zc";
-    string str2="ad";
-    if(checkSubSeq(str1,str2)){
-        cout<<"true";
-    }else{
-        cout<<"false";
+int main() {
+    string str1 = "zc";
+    string str2 = "ad"; 
+
+    if (checkSubSeq(str1, str2)) {
+        cout << "true";
+    } else {
+        cout << "false";
     }
 }
