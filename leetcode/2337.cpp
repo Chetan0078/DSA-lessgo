f169a9af8fb1f3b29413f66a50d72bdc2b9c20d6
chetan <vaishnavchetan701@gmail.com>
2024-12-05 19:26:39 +0530

    files are updated

--- leetcode/2337.cpp
+++ leetcode/2337.cpp
@@ -1,26 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
 bool canChange(string start, string target) {
-    // int i=0,j=start.size();
-    // while(i<start.size()){
-    //     if(start[i]=='L') break;
-    //     i++;
+
+    //BRUTEFORCE
+    // for(int i =1;i<start.size();i++){
+    //     for(int k=start.size()-1;k>0;k--){
+    //         if(start[k]=='L'&& start[k-1]=='_')swap(start[k],start[k-1]);
+    //         if(start[k]=='R'&& start[k+1]=='_')swap(start[k],start[k+1]);
+    //         if(start == target){
+    //             return true;
+    //         }
+    //     }
     // }
-    // while(j>0){
-    //     if(start[j]=='R') break;
-    //     j--;
-    // }
-    for(int i =1;i<start.size();i++){
-        for(int k=start.size()-1;k>0;k--){
-            if(start[k]=='L'&& start[k-1]=='_')swap(start[k],start[k-1]);
-            if(start[k]=='R'&& start[k+1]=='_')swap(start[k],start[k+1]);
-            if(start == target){
-                return true;
-            }
+    // cout<<start;
+    // return false;
+
+// optimal approach
+    int i = 0, j = 0;
+    int n = start.size();
+
+    while (i < n || j < n) {
+        while (i < n && start[i] == '_') i++;
+     
+        while (j < n && target[j] == '_') j++;
+
+        if ((i < n) != (j < n)) return false;
+
+        if (i < n && j < n) {
+            if (start[i] != target[j]) return false;
+            if (start[i] == 'L' && i < j) return false; 
+            if (start[i] == 'R' && i > j) return false; 
         }
+
+        i++;
+        j++;
     }
-    cout<<start;
-    return false;
+
+    return true;
+
 }
 
 int main(){
