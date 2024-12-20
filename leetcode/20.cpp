63234191bbfed3a3a73a7c6e61ec014ef36852e4
chetan <vaishnavchetan701@gmail.com>
2024-12-20 22:33:35 +0530

    new learnings added

---
+++ leetcode/20.cpp
@@ -0,0 +1,31 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        string stack; // Use a string as a makeshift stack
+
+        for (char c : s) {
+            // Push opening brackets to the stack
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push_back(c);
+            } else {
+                // Check if the stack is empty or doesn't match the closing bracket
+                if (stack.empty() || 
+                    (c == ')' && stack.back() != '(') || 
+                    (c == ']' && stack.back() != '[') || 
+                    (c == '}' && stack.back() != '{')) {
+                    return false;
+                }
+                stack.pop_back(); // Remove the matching opening bracket
+            }
+        }
+
+        // If the stack is empty, all brackets were matched
+        return stack.empty();
+    }
+};
+int main(){
+    
+}
