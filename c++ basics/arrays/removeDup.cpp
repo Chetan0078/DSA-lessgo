c8711070a30e5ac3ebf5c96532666c91b7133d68
chetan <vaishnavchetan701@gmail.com>
2024-12-22 21:11:34 +0530

    file is updated

--- c++ basics/arrays/removeDup.cpp
+++ c++ basics/arrays/removeDup.cpp
@@ -1,16 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int arr[]= {1,1,1,3,3,3,5,5,5,7,7,7,7,7,7,8};
-    int i =0;
-    for(int j=1;j<16;j++){
-        if(arr[i]!=arr[j]){
-            arr[i+1]=arr[j];
+int main()
+{
+    int arr[] = {1, 1, 1, 3, 3, 3, 5, 5, 5, 7, 7, 7, 7, 7, 7, 8};
+    int i = 0;
+    for (int j = 1; j < 16; j++)
+    {
+        if (arr[i] != arr[j])
+        {
+            arr[i + 1] = arr[j];
             i++;
         }
     }
-    for(int j=0;j<=i;j++){
-        cout<<arr[j]<<" ";
+    for (int j = 0; j <= i; j++)
+    {
+        cout << arr[j] << " ";
     }
 }
