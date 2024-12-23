78feebef80b16fd67caf0b7c21a352fbafd729f0
chetan <vaishnavchetan701@gmail.com>
2024-12-23 22:31:42 +0530

    new learnings added

---
+++ c++ basics/arrays/longestSubarrwithK.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestSubarrwithK(vector<int> &arr,int k){
+
+}
+
+int main(){
+    vector<int> arr = {1,2,3,2,1,1,1,1,1,4,6};
+    int k = 6;
+    cout<<longestSubarrwithK(arr,k);
+}
