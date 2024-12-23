ee3d966ac1ac2562b1e5ef7dda13e621e4bb51cb
chetan <vaishnavchetan701@gmail.com>
2024-12-24 00:00:16 +0530

    file is updated

--- c++ basics/arrays/longestSubarrwithK.cpp
+++ c++ basics/arrays/longestSubarrwithK.cpp
@@ -1,12 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int longestSubarrwithK(vector<int> &arr,int k){
+int longestSubarrwithK(vector<int> &arr,int k){//optimal approch
+    int i = 0,j = 0;
+    int sum = arr[0];
+    int result=0;
+    int n = arr.size();
+    while(j<n){
+        j++;
+        while(i<j&&sum>k){
+            sum-=arr[i];
+            i++;
+        }
+        if(j<n) sum += arr[j];
+        if(sum==k){
+            result = max(result,j-i+1);
+        }
+    }
+    return result;
+}
 
+int longarrK(vector<int> &arr,int k){//brute
+    int result=0;
+    int sum = 0;
+    for(int i=0;i<arr.size();i++){
+        for(int j=0;j<i;j++){
+            sum +=arr[j];
+            if(sum == k)result = i-j+1;
+        }
+    }
 }
 
 int main(){
-    vector<int> arr = {1,2,3,2,1,1,1,1,1,4,6};
+    vector<int> arr = {1,2,3,-2,1,1,1,1,1,1,2,6};
     int k = 6;
     cout<<longestSubarrwithK(arr,k);
 }
