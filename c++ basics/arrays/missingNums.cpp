ab2eca76b9855bbf6bef7680b21d55b387a220ad
chetan <vaishnavchetan701@gmail.com>
2024-12-29 00:01:10 +0530

    file added

--- c++ basics/arrays/missingNums.cpp
+++ c++ basics/arrays/missingNums.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int missingNum(vector<int> nums)
-{
+{ // sum approach
     int n = nums.size();
     int totalSum = n * (n + 1) / 2;
     int actualSum = 0;
@@ -12,9 +13,18 @@ int missingNum(vector<int> nums)
     }
     return totalSum - actualSum;
 }
+int missinum(vector<int> nums){//it fail when the array is in not sorted so we have to sort the array first
+    int xorr = 0;
+    int n= nums.size();
+    for(int i = 0;i<nums[n-1];i++){
+        xorr = nums[i]^i;
+        if(xorr!=0)return i;
+    }
+    return -1;  
+}
 
 int main()
 {
-    vector<int> arr = {0, 1, 2, 4, 5, 6, 7, 8};
-    cout << missingNum(arr);
+    vector<int> arr = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    cout << missinum(arr);
 }
