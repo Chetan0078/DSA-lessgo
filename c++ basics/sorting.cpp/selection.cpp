310996aec93c1439742d1c168924ca6ab9967eef
chetan <vaishnavchetan701@gmail.com>
2025-01-19 21:53:58 +0530

    file is updated

--- c++ basics/sorting.cpp/selection.cpp
+++ c++ basics/sorting.cpp/selection.cpp
@@ -4,19 +4,17 @@ eg. arr=4,6,76,3,1
 in first iteration the 1 got selected and placed at arr[0] and swapped with 4
 then we run same function for arr[1] to arr[n-1]
 so over main loop will run for i=0 to i=n-2 (because last array element don't need to be sorted, it autometically get)
+
 over nested loop will run for j=i to j=n-1 (every time the starting elements are being sorted)
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void selection(int x, int arr[])
-{
-    for (int i = 0; i < x - 2; i++)
-    {
+void selection(int x, int arr[]){
+    for (int i = 0; i < x - 2; i++){
         int min = i;
-        for (int j = i; j <= x - 1; j++)
-        {
+        for (int j = i; j <= x - 1; j++){
             if (arr[j] < arr[min])
                 min = j;
         }
