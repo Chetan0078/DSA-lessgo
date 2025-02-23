7cb5b51424cebea75c98351db4ce85c10782783d
chetan <vaishnavchetan701@gmail.com>
2025-02-23 17:41:49 +0530

    file updated

--- leetcode/3160.cpp
+++ leetcode/3160.cpp
@@ -1,22 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+    vector<int> queryResults(int limit, vector<vector<int>> &queries)
+    {
         unordered_map<int, int> g;
         unordered_map<int, int> cnt;
         vector<int> ans;
-        for (auto& q : queries) {
+        for (auto &q : queries)
+        {
             int x = q[0], y = q[1];
             cnt[y]++;
-            
+
             g[x] = y;
             ans.push_back(cnt.size());
         }
         return ans;
     }
 };
-int main(){
-    
+int main()
+{
 }
