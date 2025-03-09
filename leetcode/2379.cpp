d17f771a5448c231e3203bcb1e2fcbeba31b7132
chetan <vaishnavchetan701@gmail.com>
2025-03-09 22:15:20 +0530

    file update

--- leetcode/2379.cpp
+++ leetcode/2379.cpp
@@ -1,27 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
-    public:
-        int minimumRecolors(string blocks, int k) {
-            int n = blocks.size();
-            int mini = INT_MAX;
-            int temp = k;
-            for(int i=0;i<k;i++){
-                if(blocks[i]=='B')temp--;
-            }
-            mini = min(mini,temp);
-            for(int i=k;i<n;i++){
-                if( blocks[i]=='W')temp++;
-                if( blocks[i-k]=='W')temp--;
-                mini = min(mini,temp);
-            }
-            return mini;
+class Solution
+{
+public:
+    int minimumRecolors(string blocks, int k)
+    {
+        int n = blocks.size();
+        int mini = INT_MAX;
+        int temp = k;
+        for (int i = 0; i < k; i++)
+        {
+            if (blocks[i] == 'B')
+                temp--;
         }
-    };
-int main(){
+        mini = min(mini, temp);
+        for (int i = k; i < n; i++)
+        {
+            if (blocks[i] == 'W')
+                temp++;
+            if (blocks[i - k] == 'W')
+                temp--;
+            mini = min(mini, temp);
+        }
+        return mini;
+    }
+};
+int main()
+{
     Solution s;
     string a = "BWWWBB";
-    cout<<s.minimumRecolors(a,6);
+    cout << s.minimumRecolors(a, 6);
     // cout<<a.size();
     // int w=0,b=0;
     // for(int i=0;i<a.size();i++){
