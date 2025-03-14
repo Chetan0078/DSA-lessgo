ae238fbfcb84775d32960c13ba7fe7247af8782f
chetan <vaishnavchetan701@gmail.com>
2025-03-14 20:56:17 +0530

    update

--- leetcode/2683.cpp
+++ leetcode/2683.cpp
@@ -1,21 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool doesValidArrayExist(vector<int>& derived) {
+    bool doesValidArrayExist(vector<int> &derived)
+    {
         int ones = 0;
-        for(int i=0;i<derived.size();i++){
-            if(derived[i]!=0)ones++;
+        for (int i = 0; i < derived.size(); i++)
+        {
+            if (derived[i] != 0)
+                ones++;
         }
-        if(ones%2!=0)return false;
-        else return true;
+        if (ones % 2 != 0)
+            return false;
+        else
+            return true;
     }
 };
 
-int main(){
+int main()
+{
     Solution s;
-    vector<int> a = {1,1,1,1,1,1,1,1,1,1,0,1};
-    if(s.doesValidArrayExist(a)){cout<<"true";}
-    else cout<<"false";
-    
+    vector<int> a = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1};
+    if (s.doesValidArrayExist(a))
+    {
+        cout << "true";
+    }
+    else
+        cout << "false";
 }
