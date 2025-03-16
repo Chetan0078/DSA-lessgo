572d9ed3ea2abf3602a87faada2b20dfdd87a0c6
chetan <vaishnavchetan701@gmail.com>
2025-03-16 22:27:17 +0530

    file is updated

--- leetcode/2981.cpp
+++ leetcode/2981.cpp
@@ -1,8 +1,8 @@
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//mine solution with little bug i'll solve it later;
+// mine solution with little bug i'll solve it later;
 /*class Solution {
 public:
     int maximumLength(string s) {
@@ -28,38 +28,47 @@ public:
                 n = list[key]-2;
                 return n;
             }
-        } 
+        }
         return n;
     }
 };*/
 
-
-class Solution {
+class Solution
+{
 public:
-    int maximumLength(string s) {
+    int maximumLength(string s)
+    {
         int n = s.size();
         int l = 0, r = n;
-        auto check = [&](int x) {
+        auto check = [&](int x)
+        {
             int cnt[26]{};
-            for (int i = 0; i < n;) {
+            for (int i = 0; i < n;)
+            {
                 int j = i + 1;
-                while (j < n && s[j] == s[i]) {
+                while (j < n && s[j] == s[i])
+                {
                     ++j;
                 }
                 int k = s[i] - 'a';
                 cnt[k] += max(0, j - i - x + 1);
-                if (cnt[k] >= 3) {
+                if (cnt[k] >= 3)
+                {
                     return true;
                 }
                 i = j;
             }
             return false;
         };
-        while (l < r) {
+        while (l < r)
+        {
             int mid = (l + r + 1) >> 1;
-            if (check(mid)) {
+            if (check(mid))
+            {
                 l = mid;
-            } else {
+            }
+            else
+            {
                 r = mid - 1;
             }
         }
